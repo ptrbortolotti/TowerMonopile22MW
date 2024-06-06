096cdc09072c54d2947b30cf0e257ec7c8bdc605
ptrbortolotti <ptrbortolotti@gmail.com>
2024-06-06 13:32:35 -0600

    update files screen recording

--- WISDEM_Example/driver.py
+++ WISDEM_Example/driver.py
@@ -8,7 +8,7 @@ from wisdem.commonse.mpi_tools import MPI
 mydir = os.path.dirname(os.path.realpath(__file__))  # get path to this file
 fname_wt_input = os.path.join(mydir, "IEA-22-280-RWT.yaml")
 fname_modeling_options = os.path.join(mydir, "modeling_options_monopile.yaml")
-fname_analysis_options = os.path.join(mydir, "analysis_options_monopile.yaml")
+fname_analysis_options = os.path.join(mydir, "analysis_options_monopile_no_opt.yaml")
 wt_opt, modeling_options, analysis_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
 
